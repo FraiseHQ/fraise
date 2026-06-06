8ad0d72ecefdae2d63b76ce0312458f634e9c8e3
Rene Jean Corneille <14303619+RonsenbergVI@users.noreply.github.com>
2026-06-07 00:03:51 +0100

    update in memory graph interface

--- internal/graph/in_memory.go
+++ internal/graph/in_memory.go
@@ -24,22 +24,43 @@ package graph
 
 import (
 	"sort"
+	"sync"
 	"time"
 
 	"github.com/RonsenbergVI/fraise/internal/containers"
+	"github.com/RonsenbergVI/fraise/internal/index"
 )
 
 type InMemoryGraph[K comparable, P float32 | float64] struct {
 	idToNodes     map[K]Node[K]
 	nodeToSources map[K]map[K]Relationship[K]
 	nodeToTargets map[K]map[K]Relationship[K]
+
+	mu sync.RWMutex
+}
+
+func NewGraph[K comparable, P float32 | float64]() *InMemoryGraph[K, P] {
+	g := &InMemoryGraph[K, P]{
+		idToNodes:     make(map[K]Node[K]),
+		nodeToSources: make(map[K]map[K]Relationship[K]),
+		nodeToTargets: make(map[K]map[K]Relationship[K]),
+	}
+	return g
+}
+
+func (g *InMemoryGraph[K, P]) Lock() {
+	g.mu.Lock()
+}
+
+func (g *InMemoryGraph[K, P]) RLock() {
+	g.mu.Lock()
 }
 
 func (g *InMemoryGraph[K, P]) Get(key K) *Node[K] {
 	return nil
 }
 
-func (d *InMemoryGraph[K, P]) Set(node *Node[K]) error {
+func (g *InMemoryGraph[K, P]) Set(node *Node[K]) error {
 	// err := d.currentSet(key, value)
 	// if err != nil {
 	// 	return err,
@@ -51,6 +72,52 @@ func (d *InMemoryGraph[K, P]) Set(node *Node[K]) error {
 func (g *InMemoryGraph[K, P]) Put(key K, node *Node[K]) error {
 
 	return nil
+}
+
+func (g *InMemoryGraph[K, P]) Delete(node *Node[K]) error {
+	return nil
+}
+
+func (g *InMemoryGraph[K, P]) AdjacencyMap() map[K]map[K]*Relationship[K] {
+
+}
+
+func (g *InMemoryGraph[K, P]) PredecessorMap() map[K]map[K]*Relationship[K] {
+
+}
+
+func (g *InMemoryGraph[K, P]) Copy() Graph[K, P] {
+
+}
+
+func (g *InMemoryGraph[K, P]) Order() int {
+
+}
+
+func (g *InMemoryGraph[K, P]) Size() int {
+
+}
+
+func (g *InMemoryGraph[K, P]) Stats() GraphStats {
+
+}
+
+func (g *InMemoryGraph[K, P]) Entities() []*Entity[K] {
+
+}
+
+func (g *InMemoryGraph[K, P]) Relationships() []*Relationship[K] {
+
+}
+
+// Returns the graph vector index
+func (g *InMemoryGraph[K, P]) GetVectorIndex() *index.Index[K, containers.Vector[P], P] {
+
+}
+
+// Returns the graph full text search index
+func (g *InMemoryGraph[K, P]) GetTextIndex() *index.Index[K, string, P] {
+
 }
 
 func (g *InMemoryGraph[K, P]) Search(keywords []string, vector containers.Vector[P], topics []string, entities []string, depth int, since time.Time, until time.Time, top int) ([]*Node[K], []P) {
@@ -80,14 +147,14 @@ func (g *InMemoryGraph[K, P]) Search(keywords []string, vector containers.Vector
 	return filtered[:limit]
 }
 
-func (d *InMemoryGraph[K, P]) gatherSeeds(keywords []string, vector containers.Vector[P]) ([]*Node[K], []P) {
+func (g *InMemoryGraph[K, P]) gatherSeeds(keywords []string, vector containers.Vector[P]) ([]*Node[K], []P) {
 	return nil, []P{}
 }
 
-func (d *InMemoryGraph[K, P]) findNeighbours(seeds []*Node[K], topics []string, entities []string, depth int) ([]*Node[K], []P) {
+func (g *InMemoryGraph[K, P]) findNeighbours(seeds []*Node[K], topics []string, entities []string, depth int) ([]*Node[K], []P) {
 	return nil, []P{}
 }
 
-func (d *InMemoryGraph[K, P]) timeFilter(nodes []*Node[K], since time.Time, until time.Time) ([]*Node[K], []P) {
+func (g *InMemoryGraph[K, P]) timeFilter(nodes []*Node[K], since time.Time, until time.Time) ([]*Node[K], []P) {
 	return nil, []P{}
 }
