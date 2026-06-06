3955b4d63cea8be00f170ec1ecd6b4dded4692a4
Rene Jean Corneille <14303619+RonsenbergVI@users.noreply.github.com>
2026-06-06 10:31:05 +0100

    move graph interaction logic to graph module

--- internal/graph/in_memory.go
+++ internal/graph/in_memory.go
@@ -22,8 +22,72 @@
 
 package graph
 
+import (
+	"sort"
+	"time"
+
+	"github.com/RonsenbergVI/fraise/internal/containers"
+)
+
 type InMemoryGraph[K comparable, P float32 | float64] struct {
 	idToNodes     map[K]Node[K]
 	nodeToSources map[K]map[K]Relationship[K]
 	nodeToTargets map[K]map[K]Relationship[K]
+}
+
+func (g *InMemoryGraph[K, P]) Get(key K) *Node[K] {
+	return nil
+}
+
+func (d *InMemoryGraph[K, P]) Set(node *Node[K]) error {
+	// err := d.currentSet(key, value)
+	// if err != nil {
+	// 	return err,
+	// }
+	// return errors
+	return nil
+}
+
+func (g *InMemoryGraph[K, P]) Put(key K, node *Node[K]) error {
+
+	return nil
+}
+
+func (g *InMemoryGraph[K, P]) Search(keywords []string, vector containers.Vector[P], topics []string, entities []string, depth int, since time.Time, until time.Time, top int) ([]*Node[K], []P) {
+	// A. Search starts with gathering seeds for the graph search.
+	// Seeds are found from
+	// 1. Vector search (top K - default = 10)
+	// 2. Matching keywords
+	seeds, scores := g.gatherSeeds(keywords, vector)
+
+	// B. Walking the graph from all searchs and uinioning the found facts
+	neighbors, scores := g.findNeighbours(seeds, topics, entities, depth)
+
+	// C. Time filtered (since or until)
+
+	filtered, scores := g.timeFilter(neighbors, since, until)
+
+	// D. Truncate search results
+
+	sort.Slice(filtered, func(i, j int) bool {
+		return filtered[i].Score > filtered[j].Score
+	})
+	limit := top
+	if limit > len(filtered) {
+		limit = len(filtered)
+	}
+
+	return filtered[:limit]
+}
+
+func (d *InMemoryGraph[K, P]) gatherSeeds(keywords []string, vector containers.Vector[P]) ([]*Node[K], []P) {
+	return nil, []P{}
+}
+
+func (d *InMemoryGraph[K, P]) findNeighbours(seeds []*Node[K], topics []string, entities []string, depth int) ([]*Node[K], []P) {
+	return nil, []P{}
+}
+
+func (d *InMemoryGraph[K, P]) timeFilter(nodes []*Node[K], since time.Time, until time.Time) ([]*Node[K], []P) {
+	return nil, []P{}
 }
