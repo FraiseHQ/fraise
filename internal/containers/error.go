2c1de0fc812c27dceb048759917fec9d4799548f
Rene Jean Corneille <14303619+RonsenbergVI@users.noreply.github.com>
2026-07-12 14:27:09 +0100

    add pq error

--- internal/containers/error.go
+++ internal/containers/error.go
@@ -2,4 +2,7 @@ package containers
 
 import "errors"
 
-var DimMismatchError = errors.New("Vector dimensions do not match.")
+var (
+	DimMismatchError         = errors.New("Vector dimensions do not match.")
+	ErrPriorityQueueCapacity = errors.New("Priority queue capacity must be strictly positive.")
+)
